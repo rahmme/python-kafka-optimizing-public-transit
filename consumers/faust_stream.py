4ff057b686e767c00af2f5a076ce9aa9de2db8fd
Melissa Rahm <arm5fe@bosch.com>
2021-04-07 14:13:44 +0200

    initial commit

---
+++ consumers/faust_stream.py
@@ -0,0 +1,69 @@
+"""Defines trends calculations for stations"""
+import logging
+
+import faust
+
+logger = logging.getLogger(__name__)
+
+
+# Faust will ingest records from Kafka in this format
+class Station(faust.Record):
+    stop_id: int
+    direction_id: str
+    stop_name: str
+    station_name: str
+    station_descriptive_name: str
+    station_id: int
+    order: int
+    red: bool
+    blue: bool
+    green: bool
+
+
+# Faust will produce records to Kafka in this format
+class TransformedStation(faust.Record):
+    station_id: int
+    station_name: str
+    order: int
+    line: str
+
+
+def transform(station):
+    line = ""
+    if station.red:
+        line = "red"
+    elif station.blue:
+        line = "blue"
+    elif station.blue:
+        line = "green"
+
+    return TransformedStation(
+        station.station_id,
+        station.station_name,
+        station.order,
+        line
+    )
+
+
+app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
+topic = app.topic("org.chicago.cta.jdbc.source.station", value_type=Station)
+out_topic = app.topic("org.chicago.cta.transformed.station",
+                      partitions=1,
+                      key_type=str,
+                      value_type=TransformedStation)
+table = app.Table(
+    "TRANSFORMED_STATION",
+    default=int,
+    partitions=1,
+    changelog_topic=out_topic,
+)
+
+@app.agent(topic)
+async def stationevent(stationevents):
+    stationevents.add_processor(transform)
+    async for se in stationevents:
+        await out_topic.send(key=se.station_id, value=se)
+
+
+if __name__ == "__main__":
+    app.main()
